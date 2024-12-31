d0552ab3ae251674fdbeeee466b37e88206353e6
root <root@DESKTOP-5O70PME>
2024-12-31 22:16:45 +0100

    Initial commit

--- Db.php
+++ Db.php
@@ -1,10 +1,15 @@
-<?php 
-class Db{
-    private $db = "seatingarrangement";
-    public function getCon(){
-	    $conn=mysqli_connect('localhost','root','', 'seatingarrangement') or die('Something Wrong!');
-	    return $conn;
+<?php
+class Db {
+    private $conn;
+    public function getCon() {
+        if (!$this->conn) {
+            // Connexion à la base de données avec le mot de passe root_password
+            $this->conn = mysqli_connect('localhost', 'root', 'root_password', 'exam_seat_db');
+
+            if (mysqli_connect_error()) {
+                die('Connection failed: ' . mysqli_connect_error());
+            }
+        }
+        return $this->conn;
     }
 }
-$conn=(new Db)->getCon();
-?>
